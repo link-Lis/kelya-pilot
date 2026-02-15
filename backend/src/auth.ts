10c2cce7c1f050105177529e76fbbec7e6515d05
link-Lis <g.ceraline@gmail.com>
2026-02-15 23:00:14 +0100

    Update auth.ts

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
